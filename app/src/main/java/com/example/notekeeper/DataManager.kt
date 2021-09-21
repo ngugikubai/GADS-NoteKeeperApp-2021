daaf799a9d73616ce779cc5d7f7c05d808ca15cc
ngugikubai <kubaibrian@gmail.com>
2021-09-21 16:39:29 +0300

    Initialized Notes Function

---
+++ app/src/main/java/com/example/notekeeper/DataManager.kt
@@ -0,0 +1,65 @@
+package com.example.notekeeper
+
+import java.util.*
+
+object DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+        initializeNotes()
+    }
+
+
+
+    private fun initializeCourses() {
+        var course = CourseInfo( "android_intents", "Android Programming with Intents")
+        courses[course.courseId] = course
+
+        course = CourseInfo("android_async", "Android Async Programming and Services")
+        courses[course.courseId] = course
+
+        course = CourseInfo("java_lang ",  "Java Fundamentals: The Java Language")
+        courses[course.courseId] = course
+
+        course = CourseInfo("java_core","Java Fundamentals: The Core Platform")
+        courses[course.courseId] = course
+    }
+
+    private fun initializeNotes() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        var note = NoteInfo(course, "Dynamic intent resolution",
+            "Wow, intents allow components to be resolved at runtime")
+        notes.add(note)
+        note = NoteInfo(course, "Delegating intents",
+            "PendingIntents are powerful; they delegate much more than just a component invocation")
+        notes.add(note)
+
+        course = CourseInfo(courseId = "android_sync", title = "Android Sync Programming and servcies")
+        note = NoteInfo(course, "Service default threads",
+            "Did you know that by default an Android Service will tie up the UI thread?")
+        notes.add(note)
+        note = NoteInfo(course, "Long running operations",
+            "Foreground Services can be tied to a notification icon")
+        notes.add(note)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        note = NoteInfo(course, "Parameters",
+            "Leverage variable-length parameter lists")
+        notes.add(note)
+        note = NoteInfo(course, "Anonymous classes",
+            "Anonymous classes simplify implementing one-use types")
+        notes.add(note)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        note = NoteInfo(course, "Compiler options",
+            "The -jar option isn't compatible with with the -cp option")
+        notes.add(note)
+        note = NoteInfo(course, "Serialization",
+            "Remember to include SerialVersionUID to assure version compatibility")
+        notes.add(note)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -0,0 +1,47 @@
+package com.example.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.navigation.ui.AppBarConfiguration
+
+import com.example.notekeeper.databinding.ActivityNoteListBinding
+import kotlinx.android.synthetic.main.content_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+        binding.fab.setOnClickListener {
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+
+        }
+
+        listNotes.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataManager.notes
+        )
+
+        listNotes.setOnItemClickListener { parent, view, position, id ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(EXTRA_NOTE_POSITION,position)
+            startActivity(activityIntent)
+        }
+
+
+    }
+}
